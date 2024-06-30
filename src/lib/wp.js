27ed9bd5a4a6aeb6fd68a214e44490e2fd0e1571
namikawayusuke <yu.namikawa18@gmail.com>
2024-06-30 15:22:42 +0900

    テスト

---
+++ src/lib/wp.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = "https://public-api.wordpress.com/wp/v2";
+const SITE_URL = "yusukenmn.wordpress.com";
+export const POSTS_ENDPOINTS_URL = `${API_BASE_URL}/sites/${SITE_URL}/posts`;
